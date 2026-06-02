efa9429ccc7652d15ecc256f5b08a0d54a82fba1
Dialogmakers International Ltd <ericroennau7@gmail.com>
2026-06-02 18:32:12 +0000

    File changes

--- src/components/public/SummerPromoBanner.jsx
+++ src/components/public/SummerPromoBanner.jsx
@@ -2,7 +2,7 @@ export default function SummerPromoBanner() {
   return (
     <section className="w-full bg-black h-[320px] md:h-[340px] lg:h-[360px] overflow-hidden">
       <img
-        src="https://media.base44.com/images/public/6a1bc26018a7bec38bc6ac4a/733ec8128_generated_image.png"
+        src="https://media.base44.com/images/public/6a1bc26018a7bec38bc6ac4a/df4dc9d91_image.png"
         alt="FLESHLAB Summer Studio Special - 50% OFF Fanclub Access"
         className="w-full h-full object-cover"
         style={{ 
