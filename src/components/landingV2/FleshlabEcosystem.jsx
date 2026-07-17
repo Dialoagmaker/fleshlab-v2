b8b9c940b022f2cb7b4b548b35cf2061cca3cd3d
Dialogmakers International Ltd <ericroennau7@gmail.com>
2026-07-17 12:56:43 +0000

    File changes

--- src/components/landingV2/FleshlabEcosystem.jsx
+++ src/components/landingV2/FleshlabEcosystem.jsx
@@ -4,7 +4,7 @@ import { ArrowRight, Camera, Clapperboard, CloudUpload, ExternalLink, HeartHands
 import MediaImage from "@/components/homeTube/MediaImage";
 import AnimatedCounter from "@/components/landingV2/AnimatedCounter";
 
-const fanImage = "https://media.base44.com/images/public/6a1bc26018a7bec38bc6ac4a/b49928468_generated_image.png";
+const fanImage = "https://media.base44.com/images/public/6a1bc26018a7bec38bc6ac4a/0aee5828b_generated_image.png";
 
 const nodes = [
   { key: "CREATE", icon: Smartphone, detail: "Studio & homemade starts" },
